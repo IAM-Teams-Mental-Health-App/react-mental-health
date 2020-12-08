1ffc877c7a73e20b6c13dc6d0b2c6a03e0e15551
Tyler Morales <moratyle@gmail.com>
2020-12-08 14:40:42 -0600

    🚧  Use static svg image for lanterns

--- src/components/Lantern.js
+++ src/components/Lantern.js
@@ -3,27 +3,27 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 // local imports
-import circle1 from '../assets/lanterns/circle_lantern.jpg';
-import circle2 from '../assets/lanterns/circle2_lantern.jpg';
-import square from '../assets/lanterns/square_lantern.jpg';
+// import circle1 from '../assets/lanterns/circle_lantern.jpg';
+// import circle2 from '../assets/lanterns/circle2_lantern.jpg';
+// import square from '../assets/lanterns/square_lantern.jpg';
 import attachFly from '../hooks/attachFly';
 
 export default function Lantern({ data }) {
   const history = useHistory();
-  let lanternImg;
+  // let lanternImg;
 
   switch (data.type) {
     case 'circle':
-      lanternImg = circle1;
+      // lanternImg = circle1;
       break;
     case 'square':
-      lanternImg = square;
+      // lanternImg = square;
       break;
     case 'circle2':
-      lanternImg = circle2;
+      // lanternImg = circle2;
       break;
     default:
-      lanternImg = circle1;
+      // lanternImg = circle1;
       break;
   }
 
