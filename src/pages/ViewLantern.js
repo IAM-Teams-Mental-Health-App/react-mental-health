a8e43abbf435b698f7caf90ec2db582de5546420
Tyler Morales <moratyle@gmail.com>
2020-12-07 23:07:38 -0600

    🚀 Add API support

--- src/pages/ViewLantern.js
+++ src/pages/ViewLantern.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { data } from '../dummy/data';
+import axios from 'axios';
+// import { data } from '../dummy/data';
 
 export default function ViewLantern() {
   const { id } = useParams();
-  const lantern = data.filter((lant) => lant.id === parseInt(id))[0];
+  // const [lantern, setLantern] = useState({ type: '', content: '', id });
+  const [lantern, setLantern] = useState({});
+  const [loading, setLoading] = useState(true);
+  // const lantern = data.filter((lant) => lant.id === parseInt(id))[0];
   // console.log(lantern);
+
+  async function getDeats() {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/post/${id}`)
+      .then((response) => {
+        setLoading(false);
+        setLantern(response.data);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  }
+  useEffect(() => {
+    getDeats();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <p>LOADING</p>
+      </main>
+    );
+  }
+
   return (
     <div>
       <h1>From another soul...</h1>
